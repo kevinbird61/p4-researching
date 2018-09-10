d5fbf084435edd473c36a67bd04b9ac7a89cfc8f
kevinbird61 <kevinbird61@gmail.com>
2018-09-10 21:40:28 +0800

    Patch - add scripts for rules

---
+++ src/bcast/scripts/add_rules.sh
@@ -0,0 +1,1 @@
+simple_switch_CLI < cli.txt
--- src/bcast/scripts/bmv2.sh
+++ src/bcast/scripts/bmv2.sh
@@ -1,9 +1,14 @@
+# make pcaps folder
+mkdir pcaps/
+
+# run switch
 sudo simple_switch \
     -i 1@s1-eth1 \
     -i 2@s1-eth2 \
     -i 3@s1-eth3 \
     -i 4@s1-eth4 \
-    --pcap --thrift-port 9090 \
+    --pcap =pcaps \
+    --thrift-port 9090 \
     --nanolog ipc:///tmp/bm-0-log.ipc \
     --device-id 0 \
     target.json \
